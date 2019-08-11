d32011af210ea4d8399b9f32e43e0785c8fd7bd7
haizad <haizadannuar@gmail.com>
2019-08-11 23:04:03 +0800

    first commit

---
+++ migrations/2019_08_10_144259_create_masters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMastersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('masters', function (Blueprint $table) {
+            $table->string('mauid')->length(5);
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+            $table->primary('mauid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('masters');
+    }
+}
+
